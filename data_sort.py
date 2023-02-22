76ce886c4e2041990b7a710631d8684f0ffd9aae
alexanderZaych <118917232+Alexander-Zaychenko@users.noreply.github.com>
2023-02-22 16:58:13 +0000

    sorting and style

--- data_sort.py
+++ data_sort.py
@@ -1,10 +1,8 @@
 from data import data
 
-
 def data_sort(data):
     new_data = {}
-    for i in data:
-        name, item, count = i.split(',')
-        new_data[name] = {item: count}
+    name, item, count = data.split(',')
+    new_data[name] = {item: count}
     print(new_data)
     data = new_data
---
+++ main.py
@@ -0,0 +1,5 @@
+from data_sort import data_sort, data
+
+for i in data:
+    data_sort(i)
+    print('')
